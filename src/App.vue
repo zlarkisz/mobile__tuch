e98515ed2f419b9e498c78ba80bd7aff39af5931
G <bufin.g@brights.cloud>
2019-04-12 14:46:44 +0300

    mobile swipe

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app" ref="appToucher">
+		<div class="box" ref="box">
+			<h3>Touch my TA-LA-LA</h3>
+		</div>
+
+		<h3 ref="statusdiv">{{ statusDiv }}</h3>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		data() {
+			return {
+				counter: 0,
+				statusDiv: ''
+			}
+		},
+		mounted() {
+			this.touchMyTaLaLa()
+		},
+		methods: {
+			touchMyTaLaLa() {
+				// body.addEventListener('touchstart', (e) => {
+				// 	this.counter++
+				// 	console.log(e.changedTouches[0].pageX);
+				// },)
+				let box = this.$refs.box
+				let statusdiv = this.$refs.statusdiv
+				let startx = 0
+				let dist = 0
+
+				box.addEventListener('touchstart', (e) => {
+					let touchobj = e.changedTouches[0]
+					startx = parseInt(touchobj.clientX)
+					this.statusDiv = `Status: touchSTART ClientX: ${startx}px`
+					e.preventDefault()
+				}, false)
+
+				box.addEventListener('touchmove', (e) => {
+					let touchobj = e.changedTouches[0]
+					let dist = parseInt(touchobj.clientX) - startx
+					this.statusDiv = `Status: touchMOVE Horizontal distance traveled: ${dist}px`
+					e.preventDefault()
+				}, false)
+
+				box.addEventListener('touchend', (e) => {
+					let touchobj = e.changedTouches[0]
+					this.statusDiv = `Status: touchEND Resting x coordinate: ${touchobj}px`
+					e.preventDefault()
+				}, false)
+			}
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	* {
+		padding : 0;
+		margin  : 0;
+	}
+
+	#app {
+		height : 100vh;
+		width  : 100vw;
+	}
+
+	.box {
+		width      : 200px;
+		height     : 300px;
+		background : plum;
+	}
+
+
 </style>
